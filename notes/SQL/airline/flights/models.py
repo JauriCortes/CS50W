5305760d0f34e9fc51a2c227c70baa08786d56d8
Jauri <jauricardenas18@gmail.com>
2022-10-22 11:12:29 -0500

    sql, models and migrations

---
+++ notes/SQL/airline/flights/models.py
@@ -0,0 +1,18 @@
+from tkinter.messagebox import RETRY
+from django.db import models
+
+# Create your models here.
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
